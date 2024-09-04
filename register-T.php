3e8b45d2d534ffe5a1de75e6fa5c207133fc6d4f
Braga <guhdobraga@gmail.com>
2024-09-04 13:49:35 -0300

    Register Template done!

---
+++ register-T.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register - Trainer</title>
+    <link rel="shortcut icon" href="public/assets/ico/bola-de-futebol.png" type="image/x-icon">
+    <link rel="stylesheet" href="public/assets/css/estilo01.css">
+</head>
+<body>
+<!--Fundo do Site-->
+<div class="bg">
+<img src="public/assets/img/motivational-soccer-texture-design-1242041139700170773.jpeg">
+
+<!--Fundo do Site-->
+
+<!--Formulário de registro-->
+<div class="register-form">
+<form>
+<div class="form-ico">
+<img src="public/assets/img/pngtree-football-championship-realistic-soccer-ball-isolated-png-image_6125491.png">    
+</div>
+<h1>Register</h1>
+<div class="form-input">
+<label id="direction">Name</label>
+<input type="text" name="nome" placeholder="Name">
+
+<label id="direction">Age</label>
+<input type="text" name="age" placeholder="Age">
+
+<label id="direction">Height</label>
+<input type="text" name="height" placeholder="Height">
+
+<label id="direction">Weight</label>
+<input type="text" name="weight" placeholder="Weight">
+
+<label id="direction">Gender</label>
+<input type="text" name="gender" placeholder="Gender">
+
+<label id="direction">CPF</label>
+<input type="text" name="cpf" placeholder="CPF">
+
+<label id="direction">RG</label>
+<input type="text" name="rg" placeholder="RG">
+
+<label id="direction">Team</label>
+<input type="text" name="team" placeholder="Team">
+
+<div class="r-button">
+<button>Register</button>
+</div>
+</div>
+</form>
+</div>
+<!--Formulário de registro-->
+</body>
+</html>
