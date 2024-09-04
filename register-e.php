744f1c2c8ff1ee753a020ee733ac12fc5d846c72
carlos <carlosroheinrich@gmail.com>
2024-09-04 15:36:56 -0300

    esporte

---
+++ register-e.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register - Trainer</title>
+    <link rel="shortcut icon" href="public/assets/ico/bola-de-futebol.png" type="image/x-icon">
+    <link rel="stylesheet" href="public/assets/css/estilo01.css">
+</head>
+<body>
+<!--Fundo do Site-->
+<div class="bg">
+<img src="public/assets/img/motivational-soccer-texture-design-1242041139700170773.jpeg">
+
+<!--Fundo do Site-->
+
+<!--Formulário de registro-->
+<div class="register-form">
+<form>
+<div class="form-ico">
+<img src="public/assets/img/pngtree-football-championship-realistic-soccer-ball-isolated-png-image_6125491.png">    
+</div>
+<h1>Register</h1>
+<div class="form-input">
+<label id="direction">Modalidade</label>
+<input type="text" name="modalidade" placeholder="Modalidade">
+
+<label id="direction">Ano da olimpiada </label>
+<input type="text" name="ano_olimpiada" placeholder="Ano da olimpiada">
+
+<div class="r-button">
+<button>Register</button>
+</div>
+</div>
+</form>
+</div>
+<!--Formulário de registro-->
+</body>
+</html>
