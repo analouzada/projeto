481bc5ec65b1753a5347adc919a4d641b02cb18f
Davi <rdavisamuel@gmail.com>
2024-09-04 09:48:33 -0300

    comit: adicionei o banco certo dessa vez

---
+++ banco/crud_esporte.sql
@@ -0,0 +1,146 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 04-Set-2024 às 14:46
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `crud_esporte`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `competidor`
+--
+
+CREATE TABLE `competidor` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `idade` varchar(255) NOT NULL,
+  `peso` varchar(255) NOT NULL,
+  `altura` varchar(255) NOT NULL,
+  `sexo` varchar(255) NOT NULL,
+  `CPF` varchar(255) NOT NULL,
+  `RG` varchar(255) NOT NULL,
+  `Equipe` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `esporte`
+--
+
+CREATE TABLE `esporte` (
+  `id` int(11) NOT NULL,
+  `modalidade` varchar(255) NOT NULL,
+  `ano_olimpiada` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `localidade`
+--
+
+CREATE TABLE `localidade` (
+  `id` int(11) NOT NULL,
+  `rua` int(255) NOT NULL,
+  `bairro` int(255) NOT NULL,
+  `numero` int(11) NOT NULL,
+  `CEP` int(11) NOT NULL,
+  `cidade` int(11) NOT NULL,
+  `estado` int(11) NOT NULL,
+  `país` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `treinador`
+--
+
+CREATE TABLE `treinador` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `idade` varchar(255) NOT NULL,
+  `altura` varchar(255) NOT NULL,
+  `peso` varchar(255) NOT NULL,
+  `CPF` varchar(255) NOT NULL,
+  `RG` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `competidor`
+--
+ALTER TABLE `competidor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `esporte`
+--
+ALTER TABLE `esporte`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `localidade`
+--
+ALTER TABLE `localidade`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `treinador`
+--
+ALTER TABLE `treinador`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `competidor`
+--
+ALTER TABLE `competidor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `esporte`
+--
+ALTER TABLE `esporte`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `localidade`
+--
+ALTER TABLE `localidade`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `treinador`
+--
+ALTER TABLE `treinador`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
