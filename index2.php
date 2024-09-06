dfa619d847ceb0f50ee8e7590c9a58f98ab8905b
anadomene <analauradomene@gmail.com>
2024-09-06 10:07:05 -0300

    hj

---
+++ index2.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register - Trainer</title>
+    <link rel="shortcut icon" href="public/assets/ico/bola-de-futebol.png" type="image/x-icon">
+    <link rel="stylesheet" href="public/assets/css/estilo02.css">
+</head>
+<body>
+<!--Fundo do Site-->
+<div class="bg">
+<img src="public/assets/img/motivational-soccer-texture-design-1242041139700170773.jpeg">
+
+<!--Fundo do Site-->
+
+<!--Formulário de registro-->
+<div class="register-form">
+<form>
+<div class="form-ico">
+    
+</div>
+
+<div class="register">
+ 
+<div class="register-item">
+    <a href="">
+    </a>  
+</div>
+
+<div class="register-item">
+    <a href="">
+    </a>
+</div> 
+
+<div class="register-item">
+    <a href="">
+    </a>
+</div> 
+
+<div class="register-item">
+    <a href="">
+    </a>
+</div> 
